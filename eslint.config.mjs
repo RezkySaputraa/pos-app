b9a3c97317057adf9aae72fbf275947d6b712830
Muhammad Rezky Aulia Saputra <rezkyaulia907@gmail.com>
2025-11-22 17:49:46 +0800

    fix: error vercel

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -19,6 +19,10 @@ const eslintConfig = [
       "build/**",
       "next-env.d.ts",
     ],
+    rules: {
+      // matiin rule unescaped entities
+      "react/no-unescaped-entities": "off",
+    },
   },
 ];
 
